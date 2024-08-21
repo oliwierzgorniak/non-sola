13eda8e6191be1c9aece6b92133e09893dbed678
Oliwier Zgórniak <oliwier.zgorniak@protonmail.com>
2024-08-21 11:51:05 +0200

    donations coded

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
-import Hero from "./Hero";
-import Navbar from "./Navbar";
+import Donations from "@/components/home/donations/Donations";
+import Hero from "../components/home/hero/Hero";
+import Navbar from "../components/home/navbar/Navbar";
 
 export default function Home() {
   return (
     <>
       <Navbar />
       <Hero />
+      <Donations />
     </>
   );
 }
