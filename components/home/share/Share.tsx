9aeace90e3a1c05c9c454b0d474b73795724a31f
Oliwier Zgórniak <oliwier.zgorniak@protonmail.com>
2024-08-22 15:16:40 +0200

    share section coded

--- components/home/share/Share.tsx
+++ components/home/share/Share.tsx
@@ -1,20 +1,26 @@
+import Button from "@/components/shared/Button";
 import Image from "next/image";
+import styles from "./share.module.css";
 
 export default function Share() {
   return (
-    <section>
+    <section className={styles.container}>
       <Image
         width={467}
         height={360}
         src={"/share.jpg"}
         alt="a family walking together"
       />
-      <div>
+      <div className={styles.textContainer}>
         <h2>Powiedz o nas w swoim kościele</h2>
         <p>
           Przyczyń się do powstawania nowych par i rodzin. Dzięki twojej pomocy
           portal może zdobyć nowych członków.
         </p>
+        <Button href="/flyer.pdf">
+          <Image width={39} height={39} src={"/pdf.svg"} alt="pdf icon" />
+          Zobacz ultokę
+        </Button>
       </div>
     </section>
   );
